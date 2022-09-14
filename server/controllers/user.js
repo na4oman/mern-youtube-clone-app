8f76df5fc0d6037caa0370f29bea6a22654ad053
Atanas Irikev <atanas.irikev@gmail.com>
2022-09-14 13:12:28 +0300

    add like video and dislike video functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import { createError } from '../error.js'
 import User from '../models/User.js'
+import Video from '../models/Video.js'
 
 export const updateUser = async (req, res, next) => {
   if (req.params.userId === req.user.id) {
@@ -74,14 +75,30 @@ export const unsubscribe = async (req, res, next) => {
 }
 
 export const like = async (req, res, next) => {
+  const userId = req.user.id
+  const videoId = req.params.videoId
+
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: userId },
+      $pull: { dislikes: userId },
+    })
+    res.status(200).json('Video has been liked.')
   } catch (error) {
     next(error)
   }
 }
 
 export const dislike = async (req, res, next) => {
+  const userId = req.user.id
+  const videoId = req.params.videoId
+
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: userId },
+      $pull: { likes: userId },
+    })
+    res.status(200).json('Video has been disliked.')
   } catch (error) {
     next(error)
   }
